c6428f359142848e6d775d1c81315355dcc5dc21
Teun Beijers <teuneboon@gmail.com>
2016-11-27 22:43:56 +0100

    several small fixes to my example filter

--- examples/xai_filter.py
+++ examples/xai_filter.py
@@ -16,104 +16,26 @@ from theme import Theme
 
 def main():
     f = Filter()
-    
-    map_blue = Color(30, 144, 255)
 
-    decent_unique = Theme(text_color=Colors.WHITE, background_color=Colors.UNIQUE, border_color=Colors.WHITE,
-                          font_size=45, alert_sound=6)
-    good_unique = Theme(text_color=Colors.UNIQUE, background_color=Color(255, 255, 255), border_color=Colors.UNIQUE,
-                        font_size=45, alert_sound=5)
-    shit_unique = Theme(text_color=Colors.UNIQUE, border_color=Colors.UNIQUE, background_color=Color(0, 0, 0, 0.5),
-                        font_size=35)
-    unique = Theme(text_color=Colors.UNIQUE, border_color=Colors.UNIQUE, font_size=40, alert_sound=3)
-    special_quest_item = Theme(text_color=Colors.WHITE, background_color=Color(0, 128, 0), font_size=45,
-                               alert_sound=1)
-    quest_item = Theme(text_color=Color(50, 230, 100), border_color=Color(74, 230, 58), font_size=45)
-    gg = Theme(text_color=Color(210, 0, 220), background_color=Colors.WHITE, border_color=Color(208, 32, 144),
-               font_size=45, alert_sound=8)
-    good_currency = Theme(text_color=Colors.WHITE, background_color=Color(30, 144, 255), border_color=Colors.WHITE,
-                          font_size=45, alert_sound=5)
-    decent_currency = Theme(text_color=Colors.BLACK, background_color=Color(255, 165, 0), border_color=Colors.BLACK,
-                            font_size=45, alert_sound=1)
-    currency = Theme(text_color=Color(163, 141, 109), background_color=Colors.BLACK, border_color=Colors.WHITE, font_size=45)
-    shit_currency = Theme(text_color=Color(163, 141, 109), background_color=Color(0, 0, 0, 180),
-                          border_color=Color(163, 141, 109), font_size=45)
-
-    shaper_maps = Theme(text_color=Colors.BLOOD_RED, background_color=Colors.WHITE, border_color=Colors.BLOOD_RED,
-                        font_size=45, alert_sound=2)
-    gg_maps = Theme(text_color=Colors.BLOOD_RED, background_color=Color(184, 218, 242), border_color=Colors.BLOOD_RED,
-                    font_size=45, alert_sound=2)
-    good_maps = Theme(text_color=Color(150, 0, 0), background_color=map_blue, border_color=Color(150, 0, 0),
-                      font_size=42, alert_sound=2)
-    ok_maps = Theme(text_color=map_blue, background_color=Color(0, 0, 0), border_color=map_blue, font_size=40,
-                    alert_sound=2)
-    maps = Theme(text_color=Colors.WHITE, background_color=Colors.BLACK, border_color=Colors.WHITE, alert_sound=2)
-
-    gg_fragment = Theme(text_color=Colors.BLACK, background_color=Colors.WHITE, border_color=Colors.BLACK, font_size=45,
-                        alert_sound=6)
-    good_fragment = Theme(text_color=Colors.BLACK, background_color=Colors.BLOOD_RED, border_color=Colors.BLACK, font_size=45,
-                          alert_sound=2)
-    fragment = Theme(text_color=Colors.BLOOD_RED, background_color=Colors.BLACK, border_color=Colors.BLOOD_RED, font_size=38,
-                     alert_sound=2)
-
-    meh_div_cards = Theme(text_color=Colors.BLACK, background_color=Color(200, 200, 200, 180), border_color=map_blue,
-                          font_size=45)
-    gg_div_cards = Theme(text_color=Colors.WHITE, background_color=Color(208, 32, 144), border_color=Colors.WHITE,
-                         font_size=45, alert_sound=5)
-    good_div_cards = Theme(text_color=Colors.WHITE, background_color=map_blue, border_color=Colors.WHITE, font_size=45,
-                           alert_sound=6)
-    shit_div_cards = Theme(text_color=Colors.BLACK, background_color=Color(200, 200, 200, 180), border_color=Colors.BLACK,
-                           font_size=35)
-    div_cards = Theme(text_color=Colors.BLACK, background_color=Color(200, 200, 200, 180), border_color=map_blue,
-                      font_size=45, alert_sound=3)
-
-    six_socket = Theme(text_color=Colors.WHITE, background_color=Colors.GRAY, border_color=Colors.WHITE, font_size=45,
-                       alert_sound=7)
-    six_socket_special = Theme(text_color=Color(255, 200, 0), background_color=Colors.GRAY, border_color=Color(255, 200, 0),
-                               font_size=45, alert_sound=7)
-
-    gg_gems = Theme(text_color=Colors.GEM, background_color=Colors.WHITE, border_color=Colors.GEM, font_size=45, alert_sound=5)
-    ok_quality_gems = Theme(border_color=Colors.GEM, font_size=45, alert_sound=1)
-    quality_gems = Theme(border_color=Colors.GEM)
-    good_gems = Theme(border_color=Colors.BLOOD_RED)
-
-    rare_jewels = Theme(text_color=Colors.YELLOW, background_color=Color(75, 75, 0), border_color=Colors.YELLOW, font_size=45)
-    rare_atlas_bases = Theme(text_color=Color(255, 255, 119), background_color=Color(74, 230, 58), border_color=Color(255, 255, 119),
-                            font_size=45, alert_sound=5)
-    rare_max_ilvl_bases = Theme(text_color=Color(0, 128, 0), background_color=Color(255, 200, 0), border_color=(0, 128, 0),
-                           font_size=45)
-    rare_decent_ilvl_bases = Theme(text_color=Color(255, 190, 0), background_color=Color(30, 90, 45), border_color=Colors.WHITE, font_size=38)
-    rare_good_bases = Theme(background_color=Color(30, 90, 45), border_color=Color(255, 255, 119), font_size=38)
-    rare_shit_bases = Theme(background_color=Color(120, 20, 20, 150), border_color=Color(150, 150, 150, 150), font_size=25)
-
-    white_atlas_bases = Theme(text_color=Color(255, 255, 119), border_color=Color(255, 255, 119),
-                              background_color=Colors.BLACK, font_size=45, alert_sound=1)
-    white_gg_bases = Theme(border_color=Color(255, 255, 0), font_size=40, background_color=Colors.BLACK)
-    white_ok_bases = Theme(border_color=Color(255, 255, 0, 180), background_color=Color(0, 0, 0, 180), font_size=38)
-
-    chromatic_item = Theme(border_color=Color(27, 162, 155), background_color=Color(107, 107, 107), font_size=36)
-    chance_item = Theme(border_color=Color(74, 230, 58))
-
-    alch_base = Theme(border_color=Color(255, 190, 0, 150), font_size=32)
-    magic_jewellery = Theme(border_color=Colors.BLACK, font_size=28, background_color=Color(0, 0, 0, 165))
-    magic_jewel = Theme(text_color=Color(0, 100, 255), background_color=Color(0, 40, 60), border_color=Color(0, 75, 250),
-                        font_size=36)
-
-    high_quality_flask = Theme(background_color=Color(75, 75, 75), border_color=Colors.WHITE, font_size=45)
-    utility_flask = Theme(background_color=Color(75, 75, 75), border_color=Colors.BLACK)
-    early_survival = Theme(border_color=Colors.BLOOD_RED, font_size=40)
-
+    # please don't shoot me for this code
+    alch_base, chance_item, chromatic_item, currency, decent_currency, decent_unique, div_cards, early_survival,\
+    fragment, gg, gg_div_cards, gg_fragment, gg_gems, gg_maps, good_currency, good_div_cards, good_fragment, good_gems,\
+    good_maps, good_unique, high_quality_flask, magic_jewel, magic_jewellery, maps, meh_div_cards, ok_maps, ok_quality_gems,\
+    quality_gems, quest_item, rare_atlas_bases, rare_decent_ilvl_bases, rare_good_bases, rare_jewels, rare_max_ilvl_bases,\
+    rare_shit_bases, shaper_maps, shit_currency, shit_div_cards, shit_unique, six_socket, six_socket_special, special_quest_item,\
+    unique, utility_flask, white_atlas_bases, white_gg_bases, white_ok_bases = get_themes()
 
     f.add(Comment('Section: #0001 - Special Stuff\n'))
     f.add(Block(theme=decent_unique,
-                         comment='Tabula, we have to put this before everything cause our 6L block will override otherwise',
-                         socket_group='W'*6,
-                         rarity='Unique'))
+                comment='Tabula, we have to put this before everything cause our 6L block will override otherwise',
+                socket_group='W'*6,
+                rarity='Unique',
+                _class='Simple Robe'))
     f.add(Block(theme=special_quest_item, _class='Quest Items', base_type='Shaper\'s Orb'))
     f.add(Block(theme=quest_item, _class=['Quest Items', 'Microtransactions']))
     # @TODO: Add more base types
-    f.add(Block(theme=gg, item_level=1, rarity='Normal', base_type=['Glass Shank', 'Driftwoord Wand'], play_alert_sound=None,
-                         comment='Make ilvl 1 of starter weapons ugly so people know they forgot their racing filter @TODO: add others'))
+    f.add(Block(theme=gg, item_level=1, rarity='Normal', base_type=['Glass Shank', 'Driftwood Wand'], play_alert_sound=None,
+                comment='Make ilvl 1 of starter weapons ugly so people know they forgot their racing filter @TODO: add others'))
 
     f.add(Comment('Section: #0002 - Labyrinth\n'))
     f.add(Block(theme=special_quest_item, base_type='Offering to the Goddess'))
@@ -125,11 +47,11 @@ def main():
 
     f.add(Comment('Section: #0004 - Uniques\n'))
     f.add(Block(theme=good_unique, item_level=84, rarity='Unique', base_type=['Infernal Sword', 'Ruby Flask'],
-                         comment='This is probably a Starforge or Dying Sun'))
+                comment='This is probably a Starforge or Dying Sun'))
     f.add(Block(theme=good_unique, item_level=82, rarity='Unique', base_type=['Vaal Gauntlets', 'Vaal Axe'],
-                         comment='Acuity+Disfavour from Atziri(They drop ilvl 82 from Atziri, div card version should never be on the ground anyway), put it here so we don\'t show other vaal gauntlets or vaal axes as GG'))
+                comment='Acuity+Disfavour from Atziri(They drop ilvl 82 from Atziri, div card version should never be on the ground anyway), put it here so we don\'t show other vaal gauntlets or vaal axes as GG'))
     f.add(Block(theme=good_unique, item_level=74, rarity='Unique', base_type='Spine Bow',
-                         comment='Reach of the Council(so we don\'t show Voltaxic as GG)'))
+                comment='Reach of the Council(so we don\'t show Voltaxic as GG)'))
     f.add(Block(theme=good_unique, comment='Uniques that are usually 1ex+', rarity='Unique',
                          base_type=['Occultist\'s Vestment', 'Jewelled Foil', 'Glorious Plate', 'Prismatic Jewel',
                                     'Citrine Amulet', 'Gladiator Plate', 'Assassin\'s Garb', 'Golden Mantle',
@@ -159,11 +81,11 @@ def main():
         drop_level = maps_list[tier][0].drop_level
 
         block_args = {'drop_level': Comparer(drop_level, '>='), '_class': 'Maps'}
-        if tier >= 15:
+        if tier >= 16:
             block_args['theme'] = shaper_maps
-        elif tier >= 12:
+        elif tier >= 13:
             block_args['theme'] = gg_maps
-        elif tier >= 9:
+        elif tier >= 10:
             block_args['theme'] = good_maps
         elif tier >= 5:
             block_args['theme'] = ok_maps
@@ -398,6 +320,97 @@ def main():
 
     with open('Xai.filter', 'w') as file:
         file.write(str(f))
+
+
+def get_themes():
+    map_blue = Color(30, 144, 255)
+    decent_unique = Theme(text_color=Colors.WHITE, background_color=Colors.UNIQUE, border_color=Colors.WHITE,
+                          font_size=45, alert_sound=6)
+    good_unique = Theme(text_color=Colors.UNIQUE, background_color=Color(255, 255, 255), border_color=Colors.UNIQUE,
+                        font_size=45, alert_sound=5)
+    shit_unique = Theme(text_color=Colors.UNIQUE, border_color=Colors.UNIQUE, background_color=Color(0, 0, 0, 0.5),
+                        font_size=35)
+    unique = Theme(text_color=Colors.UNIQUE, border_color=Colors.UNIQUE, font_size=40, alert_sound=3)
+    special_quest_item = Theme(text_color=Colors.WHITE, background_color=Color(0, 128, 0), font_size=45,
+                               alert_sound=1)
+    quest_item = Theme(text_color=Color(50, 230, 100), border_color=Color(74, 230, 58), font_size=45)
+    gg = Theme(text_color=Color(210, 0, 220), background_color=Colors.WHITE, border_color=Color(208, 32, 144),
+               font_size=45, alert_sound=8)
+    good_currency = Theme(text_color=Colors.WHITE, background_color=Color(30, 144, 255), border_color=Colors.WHITE,
+                          font_size=45, alert_sound=5)
+    decent_currency = Theme(text_color=Colors.BLACK, background_color=Color(255, 165, 0), border_color=Colors.BLACK,
+                            font_size=45, alert_sound=1)
+    currency = Theme(text_color=Color(163, 141, 109), background_color=Colors.BLACK, border_color=Colors.WHITE,
+                     font_size=45)
+    shit_currency = Theme(text_color=Color(163, 141, 109), background_color=Color(0, 0, 0, 180),
+                          border_color=Color(163, 141, 109), font_size=45)
+    shaper_maps = Theme(text_color=Colors.BLOOD_RED, background_color=Colors.WHITE, border_color=Colors.BLOOD_RED,
+                        font_size=45, alert_sound=2)
+    gg_maps = Theme(text_color=Colors.BLOOD_RED, background_color=Color(184, 218, 242), border_color=Colors.BLOOD_RED,
+                    font_size=45, alert_sound=2)
+    good_maps = Theme(text_color=Color(150, 0, 0), background_color=map_blue, border_color=Color(150, 0, 0),
+                      font_size=42, alert_sound=2)
+    ok_maps = Theme(text_color=map_blue, background_color=Color(0, 0, 0), border_color=map_blue, font_size=40,
+                    alert_sound=2)
+    maps = Theme(text_color=Colors.WHITE, background_color=Colors.BLACK, border_color=Colors.WHITE, alert_sound=2)
+    gg_fragment = Theme(text_color=Colors.BLACK, background_color=Colors.WHITE, border_color=Colors.BLACK, font_size=45,
+                        alert_sound=6)
+    good_fragment = Theme(text_color=Colors.BLACK, background_color=Colors.BLOOD_RED, border_color=Colors.BLACK,
+                          font_size=45,
+                          alert_sound=2)
+    fragment = Theme(text_color=Colors.BLOOD_RED, background_color=Colors.BLACK, border_color=Colors.BLOOD_RED,
+                     font_size=38,
+                     alert_sound=2)
+    meh_div_cards = Theme(text_color=Colors.BLACK, background_color=Color(200, 200, 200, 180), border_color=map_blue,
+                          font_size=45)
+    gg_div_cards = Theme(text_color=Colors.WHITE, background_color=Color(208, 32, 144), border_color=Colors.WHITE,
+                         font_size=45, alert_sound=5)
+    good_div_cards = Theme(text_color=Colors.WHITE, background_color=map_blue, border_color=Colors.WHITE, font_size=45,
+                           alert_sound=6)
+    shit_div_cards = Theme(text_color=Colors.BLACK, background_color=Color(200, 200, 200, 180),
+                           border_color=Colors.BLACK,
+                           font_size=35)
+    div_cards = Theme(text_color=Colors.BLACK, background_color=Color(200, 200, 200, 180), border_color=map_blue,
+                      font_size=45, alert_sound=3)
+    six_socket = Theme(text_color=Colors.WHITE, background_color=Colors.GRAY, border_color=Colors.WHITE, font_size=45,
+                       alert_sound=7)
+    six_socket_special = Theme(text_color=Color(255, 200, 0), background_color=Colors.GRAY,
+                               border_color=Color(255, 200, 0),
+                               font_size=45, alert_sound=7)
+    gg_gems = Theme(text_color=Colors.GEM, background_color=Colors.WHITE, border_color=Colors.GEM, font_size=45,
+                    alert_sound=5)
+    ok_quality_gems = Theme(border_color=Colors.GEM, font_size=45, alert_sound=1)
+    quality_gems = Theme(border_color=Colors.GEM)
+    good_gems = Theme(border_color=Colors.BLOOD_RED)
+    rare_jewels = Theme(text_color=Colors.YELLOW, background_color=Color(75, 75, 0), border_color=Colors.YELLOW,
+                        font_size=45)
+    rare_atlas_bases = Theme(text_color=Color(255, 255, 119), background_color=Color(74, 230, 58),
+                             border_color=Color(255, 255, 119),
+                             font_size=45, alert_sound=5)
+    rare_max_ilvl_bases = Theme(text_color=Color(0, 128, 0), background_color=Color(255, 200, 0),
+                                border_color=(0, 128, 0),
+                                font_size=45)
+    rare_decent_ilvl_bases = Theme(text_color=Color(255, 190, 0), background_color=Color(30, 90, 45),
+                                   border_color=Colors.WHITE, font_size=38)
+    rare_good_bases = Theme(background_color=Color(30, 90, 45), border_color=Color(255, 255, 119), font_size=38)
+    rare_shit_bases = Theme(background_color=Color(120, 20, 20, 150), border_color=Color(150, 150, 150, 150),
+                            font_size=25)
+    white_atlas_bases = Theme(text_color=Color(255, 255, 119), border_color=Color(255, 255, 119),
+                              background_color=Colors.BLACK, font_size=45, alert_sound=1)
+    white_gg_bases = Theme(border_color=Color(255, 255, 0), font_size=40, background_color=Colors.BLACK)
+    white_ok_bases = Theme(border_color=Color(255, 255, 0, 180), background_color=Color(0, 0, 0, 180), font_size=38)
+    chromatic_item = Theme(border_color=Color(27, 162, 155), background_color=Color(107, 107, 107), font_size=36)
+    chance_item = Theme(border_color=Color(74, 230, 58))
+    alch_base = Theme(border_color=Color(255, 190, 0, 150), font_size=32)
+    magic_jewellery = Theme(border_color=Colors.BLACK, font_size=28, background_color=Color(0, 0, 0, 165))
+    magic_jewel = Theme(text_color=Color(0, 100, 255), background_color=Color(0, 40, 60),
+                        border_color=Color(0, 75, 250),
+                        font_size=36)
+    high_quality_flask = Theme(background_color=Color(75, 75, 75), border_color=Colors.WHITE, font_size=45)
+    utility_flask = Theme(background_color=Color(75, 75, 75), border_color=Colors.BLACK)
+    early_survival = Theme(border_color=Colors.BLOOD_RED, font_size=40)
+    return alch_base, chance_item, chromatic_item, currency, decent_currency, decent_unique, div_cards, early_survival, fragment, gg, gg_div_cards, gg_fragment, gg_gems, gg_maps, good_currency, good_div_cards, good_fragment, good_gems, good_maps, good_unique, high_quality_flask, magic_jewel, magic_jewellery, maps, meh_div_cards, ok_maps, ok_quality_gems, quality_gems, quest_item, rare_atlas_bases, rare_decent_ilvl_bases, rare_good_bases, rare_jewels, rare_max_ilvl_bases, rare_shit_bases, shaper_maps, shit_currency, shit_div_cards, shit_unique, six_socket, six_socket_special, special_quest_item, unique, utility_flask, white_atlas_bases, white_gg_bases, white_ok_bases
+
 
 if __name__ == '__main__':
     main()
