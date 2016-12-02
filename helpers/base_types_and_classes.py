1b78eee2f051f19b3f1d9475053b6001119ee7ff
Teun Beijers <teuneboon@gmail.com>
2016-12-02 20:37:24 +0100

    added Spiraled Foil as a gg base

--- helpers/base_types_and_classes.py
+++ helpers/base_types_and_classes.py
@@ -14,7 +14,8 @@ good_spell_bases = ['Void Sceptre', 'Opal Sceptre', 'Profane Wand', 'Opal Wand',
 # not literally armour(aka red) bases, but stuff you wear like boots/gloves/chest, stuff you need ilvl 84 for
 good_armour_bases = ['Saintly Chainmail', 'Harmonic Spirit Shield', 'Fossilised Spirit Shield', 'Titan Gauntlets',
                      'Slink Gloves', 'Eternal Burgonet', 'Lion Pelt', 'Titan Greaves', 'Slink Boots']
-gg_phys_bases = ['Vaal Axe', 'Coronal Maul', 'Harbinger Bow', 'Ambusher', 'Imbued Wand', 'Prophecy Wand', 'Jewelled Foil']
+gg_phys_bases = ['Vaal Axe', 'Coronal Maul', 'Harbinger Bow', 'Ambusher', 'Imbued Wand', 'Prophecy Wand', 'Jewelled Foil',
+                 'Spiraled Foil']
 good_phys_bases = ['Lion Sword', 'Vaal Greatsword', 'Exquisite Blade', 'Fleshripper', 'Gemini Claw', 'Demon Dagger',
                    'Imperial Skean', 'Tiger Hook', 'Runic Hatchet', 'Behemoth Mace', 'Eternal Sword',
                    'Eclipse Staff', 'Maelström Staff', 'Judgement Staff', 'Dragoon Sword', 'Vaal Hatchet']
