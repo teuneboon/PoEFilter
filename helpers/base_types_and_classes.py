e8544279b5fb9c71dd214909cacb925275d987d7
Teun Beijers <teuneboon@gmail.com>
2016-12-02 19:06:18 +0100

    jewelled foils now gg base

--- helpers/base_types_and_classes.py
+++ helpers/base_types_and_classes.py
@@ -14,9 +14,9 @@ good_spell_bases = ['Void Sceptre', 'Opal Sceptre', 'Profane Wand', 'Opal Wand',
 # not literally armour(aka red) bases, but stuff you wear like boots/gloves/chest, stuff you need ilvl 84 for
 good_armour_bases = ['Saintly Chainmail', 'Harmonic Spirit Shield', 'Fossilised Spirit Shield', 'Titan Gauntlets',
                      'Slink Gloves', 'Eternal Burgonet', 'Lion Pelt', 'Titan Greaves', 'Slink Boots']
-gg_phys_bases = ['Vaal Axe', 'Coronal Maul', 'Harbinger Bow', 'Ambusher', 'Imbued Wand', 'Prophecy Wand']
+gg_phys_bases = ['Vaal Axe', 'Coronal Maul', 'Harbinger Bow', 'Ambusher', 'Imbued Wand', 'Prophecy Wand', 'Jewelled Foil']
 good_phys_bases = ['Lion Sword', 'Vaal Greatsword', 'Exquisite Blade', 'Fleshripper', 'Gemini Claw', 'Demon Dagger',
-                   'Imperial Skean', 'Tiger Hook', 'Jewelled Foil', 'Runic Hatchet', 'Behemoth Mace', 'Eternal Sword',
+                   'Imperial Skean', 'Tiger Hook', 'Runic Hatchet', 'Behemoth Mace', 'Eternal Sword',
                    'Eclipse Staff', 'Maelström Staff', 'Judgement Staff', 'Dragoon Sword', 'Vaal Hatchet']
 # stuff that can't be used as a spell base... and staves
 melee_only_classes = ['One Hand Maces', 'One Hand Axes', 'One Hand Swords', 'Claws', 'Staves', 'Two Hand']
